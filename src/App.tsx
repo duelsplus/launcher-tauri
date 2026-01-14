23127a40eec29b04484ac01ec9584a708138af9b
leeteral <kneesdev@naver.com>
2026-01-14 15:21:44 +0200

    fix: disable webkit right-click context menu

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,13 @@ function Content() {
 
   useEffect(() => {
     showWindow();
+
+    const handleContextMenu = (e: MouseEvent) => e.preventDefault();
+    document.addEventListener("contextmenu", handleContextMenu);
+
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
   }, []);
 
   if (!isOnboarded) {
